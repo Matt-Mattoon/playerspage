d1159868080144ed5c1f422f9cdd87c9ed94475e
Kyree Graham <141266977+Truecoderalert@users.noreply.github.com>
2024-03-13 14:37:26 -0400

    Add files via upload

---
+++ models_attribute.py
@@ -0,0 +1,38 @@
+db = 'atheletes'
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Attribute:
+    def __init__(self , data):
+        self.id = data['id']
+        self.name=data['name']
+        self.position = data['position']
+        self.school=data['school']
+        self.speed = data['speed']
+        self.top_strength = data['top_strength']
+        self.bottom_strength = data['bottom_strength']
+        self.user_id = data['user_id']
+        
+        
+        
+    @classmethod
+    def create(cls,data):
+        query = 'insert into attributes (profile_picture , name  , school , top_strength , bottom_strength , speed  , position ,   user_id) values (%(profile_picture)s     ,   %(name)s , %(school)s ,  %(top_strength)s , %(bottom_strength)s , %(speed)s , %(position)s ,  %(user_id)s )'
+        return connectToMySQL(db).query_db(query ,  data)
+    
+    @classmethod
+    def get_one(cls , data):
+        query = """select * from attributes 
+        join users ON users.id = attributes.user_id
+        Where attributes.id = %(id)s
+        """
+        results = connectToMySQL(db).query_db(query ,  data)
+        attributes = []
+        for attribute in results:
+            attributes.append(attribute)
+        return attributes
+    
+
+        
+        
+        
+    
