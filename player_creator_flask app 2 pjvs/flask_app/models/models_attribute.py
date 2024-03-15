3107375d031e2fa2c0607524fd99a5d50c41e56f
Matt <matthew.mattoon@yahoo.com>
2024-03-14 23:08:48 -0400

    replaced outdated files with files that were uploaded

--- player_creator_flask app 2 pjvs/flask_app/models/models_attribute.py
+++ player_creator_flask app 2 pjvs/flask_app/models/models_attribute.py
@@ -4,16 +4,19 @@ from flask_app.config.mysqlconnection import connectToMySQL
 class Attribute:
     def __init__(self , data):
         self.id = data['id']
+        self.name=data['name']
+        self.position = data['position']
+        self.school=data['school']
         self.speed = data['speed']
         self.top_strength = data['top_strength']
         self.bottom_strength = data['bottom_strength']
         self.user_id = data['user_id']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+        
+        
         
     @classmethod
     def create(cls,data):
-        query = 'insert into attributes (top_strength , bottom_strength , speed , user_id) values ( %(top_strength)s , %(bottom_strength)s , %(speed)s , %(user_id)s )'
+        query = 'insert into attributes (profile_picture , name  , school , top_strength , bottom_strength , speed  , position ,   user_id) values (%(profile_picture)s     ,   %(name)s , %(school)s ,  %(top_strength)s , %(bottom_strength)s , %(speed)s , %(position)s ,  %(user_id)s )'
         return connectToMySQL(db).query_db(query ,  data)
     
     @classmethod
@@ -27,4 +30,9 @@ class Attribute:
         for attribute in results:
             attributes.append(attribute)
         return attributes
+    
+
+        
+        
+        
     
