17894cd00abad110a5b234c9439095f40ba115bb
Matt <matthew.mattoon@yahoo.com>
2024-03-18 21:31:13 -0400

    Integrated changes from your uploads, app wide improvements!

--- flask_app/models/models_user.py
+++ flask_app/models/models_user.py
@@ -3,8 +3,9 @@ from flask import flash #type:ignore
 import re	# the regex module
 # create a regular expression object that we'll use later   
 r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$'
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
-db = 'atheletes'
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = r'[^A-Za-z]'
+db = 'athletes'
 class User:
     def __init__(self,data):
         self.id = data['id']
@@ -42,5 +43,43 @@ class User:
         if len(users['password']) < 1:
             flash("Please enter a password")
             is_valid = False
-            
+        return is_valid
+    
+    @staticmethod
+    def validate_login(data):
+        is_valid = True
+        #check email format
+        if not EMAIL_REGEX.match(data['email']):
+            flash('Invalid Email')
+            is_valid = False
+        #check password for required length, number included, capital included
+        if not len(data['password']) > 7 or re.search('[0-9]', data['password']) is None or re.search('[A-Z]', data['password']) is None:
+            flash('Password must contain at least 8 characters, a number, and a capital letter.')
+            is_valid = False
+        return is_valid
+    
+    @staticmethod
+    def validate_register(data):
+        is_valid = True
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        results = connectToMySQL(db).query_db(query, data)
+        if results:
+            if len(results) >= 1:
+                flash('Email already in use.')
+                is_valid = False
+        if not EMAIL_REGEX.match(data['email']):
+            flash('Invalid Email.')
+            is_valid = False
+        if not len(data['first_name']) >= 2 or re.search(NAME_REGEX, data['first_name']):
+            flash('First name must be at least 2 characters and can only contain letters')
+            is_valid = False
+        if not len(data['last_name']) >= 2 or re.search(NAME_REGEX, data['last_name']):
+            flash('Last name must be at least 2 characters and can only contain letters')
+            is_valid = False
+        if not len(data['password']) > 7 or re.search('[0-9]', data['password']) is None or re.search('[A-Z]', data['password']) is None:
+            flash('Password must contain at least 8 characters, a number, and a capital letter.')
+            is_valid = False
+        if data['password'] != data['confirm_password']:
+            flash('password does not match')
+            is_valid = False
         return is_valid
